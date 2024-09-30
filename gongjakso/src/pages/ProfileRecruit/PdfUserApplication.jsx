a7063d9f6277509f0cf6c1a9df8f8767d2173680
Hanseul Lee <lhs010107@gmail.com>
2024-09-30 14:06:31 +0900

    :lipstick: Add portfolio box UI
    #271 지원서 양식 기본틀 완성

--- gongjakso/src/pages/ProfileRecruit/PdfUserApplication.jsx
+++ gongjakso/src/pages/ProfileRecruit/PdfUserApplication.jsx
@@ -1,28 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import * as S from './PdfUserApplicationStyled';
 import * as T from '../../features/modal/ApplyModal.styled';
+import * as U from '../Portfolio/Portfolio.Styled';
 import { useParams } from 'react-router-dom';
 import Logo from '../../assets/images/applicationLogo.svg';
 import { getMyApplication } from '../../service/apply_service';
-import { getMyPortfolio } from '../../service/post_service';
+import { getPortfolio } from '../../service/portfolio_service';
 
 const PdfUserApplication = () => {
     const { id } = useParams();
 
     const [myApp, setmyApp] = useState([]);
-    const [portfolioData, setportfolioData] = useState([]);
+    const [portId, setPortId] = useState();
+    const [portfolio, setPortfolio] = useState([]);
 
     useEffect(() => {
         // [GET] 특정 지원자 지원서 조회 API
         getMyApplication(id).then(res => {
             setmyApp(res?.data);
-        });
-
-        // [GET] 내 포트폴리오 리스트 조회 API
-        getMyPortfolio().then(res => {
-            setportfolioData(res?.data);
+            setPortId(res?.data.portfolio_id);
         });
     }, [id]);
+
+    useEffect(() => {
+        if (portId) {
+            // [GET] 포트폴리오 상세 조회 API
+            getPortfolio(portId).then(res => {
+                setPortfolio(res?.data);
+            });
+        }
+    }, [portId]);
 
     return (
         <>
@@ -54,6 +61,159 @@ const PdfUserApplication = () => {
                     <S.SubTitle>지원이유</S.SubTitle>
                     <S.ContentBox>{myApp?.body}</S.ContentBox>
                 </S.DetailBox>
+
+                {!myApp?.is_private && (
+                    <div>
+                        <S.DetailBox>
+                            <S.SubTitle>학력</S.SubTitle>
+                            {portfolio?.data?.educationList?.map(
+                                (education, index) => (
+                                    <S.BoxDetail key={index}>
+                                        <S.InputContainer>
+                                            <S.PortBox $w="50%">
+                                                {education.school ||
+                                                    '학교 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.grade ||
+                                                    '학년 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.state ||
+                                                    '상태 정보 없음'}
+                                            </S.PortBox>
+                                        </S.InputContainer>
+                                    </S.BoxDetail>
+                                ),
+                            )}
+                        </S.DetailBox>
+
+                        <S.DetailBox>
+                            <S.SubTitle>경력사항</S.SubTitle>
+                            {portfolio?.data?.workList?.map((work, index) => (
+                                <S.BoxDetail key={index}>
+                                    <S.InputContainer>
+                                        <S.PortBox $w="58%">
+                                            (회사명){' '}
+                                            {work.company || '정보 없음'}
+                                        </S.PortBox>
+                                        <S.PortBox $w="40%">
+                                            (부서명/직책) {work.partition || ''}
+                                        </S.PortBox>
+                                    </S.InputContainer>
+
+                                    <S.InputContainer>
+                                        <S.FlexPortBox $w="32%">
+                                            <span>입사일</span>
+                                            <span>
+                                                {work.enteredAt || '정보 없음'}
+                                            </span>
+                                        </S.FlexPortBox>
+                                        <S.FlexPortBox $w="32%">
+                                            <span>퇴사일</span>
+                                            <span>
+                                                {work.exitedAt || '미정'}
+                                            </span>
+                                        </S.FlexPortBox>
+                                        <S.PortBox $w="32%">
+                                            {work.isActive ? '재직 중' : '퇴사'}
+                                        </S.PortBox>
+                                    </S.InputContainer>
+
+                                    <S.InputContainer>
+                                        <S.WorkContent>
+                                            {work.detail}
+                                        </S.WorkContent>
+                                    </S.InputContainer>
+                                </S.BoxDetail>
+                            ))}
+                        </S.DetailBox>
+
+                        <S.DetailBox>
+                            <S.SubTitle>대외활동</S.SubTitle>
+                            {portfolio?.data?.educationList?.map(
+                                (education, index) => (
+                                    <S.BoxDetail key={index}>
+                                        <S.InputContainer>
+                                            <S.PortBox $w="70%">
+                                                {education.school ||
+                                                    '학교 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="22rem">
+                                                {education.grade ||
+                                                    '학년 정보 없음'}
+                                            </S.PortBox>
+                                        </S.InputContainer>
+                                    </S.BoxDetail>
+                                ),
+                            )}
+                        </S.DetailBox>
+
+                        <S.DetailBox>
+                            <S.SubTitle>수상경력</S.SubTitle>
+                            {portfolio?.data?.educationList?.map(
+                                (education, index) => (
+                                    <S.BoxDetail key={index}>
+                                        <S.InputContainer>
+                                            <S.PortBox $w="50%">
+                                                {education.school ||
+                                                    '학교 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.grade ||
+                                                    '학년 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.state ||
+                                                    '상태 정보 없음'}
+                                            </S.PortBox>
+                                        </S.InputContainer>
+                                    </S.BoxDetail>
+                                ),
+                            )}
+                        </S.DetailBox>
+
+                        <S.DetailBox>
+                            <S.SubTitle>자격증</S.SubTitle>
+                            {portfolio?.data?.educationList?.map(
+                                (education, index) => (
+                                    <S.BoxDetail key={index}>
+                                        <S.InputContainer>
+                                            <S.PortBox $w="50%">
+                                                {education.school ||
+                                                    '학교 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.grade ||
+                                                    '학년 정보 없음'}
+                                            </S.PortBox>
+                                            <S.PortBox $w="24rem">
+                                                {education.state ||
+                                                    '상태 정보 없음'}
+                                            </S.PortBox>
+                                        </S.InputContainer>
+                                    </S.BoxDetail>
+                                ),
+                            )}
+                        </S.DetailBox>
+
+                        <S.DetailBox>
+                            <S.SubTitle>SNS</S.SubTitle>
+                            {portfolio?.data?.educationList?.map(
+                                (education, index) => (
+                                    <S.BoxDetail key={index}>
+                                        <S.InputContainer>
+                                            <S.PortBox $w="90%">
+                                                {education.school ||
+                                                    '학교 정보 없음'}
+                                            </S.PortBox>
+                                        </S.InputContainer>
+                                    </S.BoxDetail>
+                                ),
+                            )}
+                        </S.DetailBox>
+                    </div>
+                )}
             </S.GlobalBox>
         </>
     );
