b7c3188e8b8fcf5e0808b63c7064a57a5e1c4bc8
soobin <subin3620@naver.com>
2024-11-14 22:52:49 +0900

    :recycle: Refactor Applied Team Page StyleUnit
    #319 지원 기록 페이지 스타일 rem 변환

--- gongjakso/src/pages/ProfileApplied/AppliedTeamStyled.jsx
+++ gongjakso/src/pages/ProfileApplied/AppliedTeamStyled.jsx
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 import { ReactComponent as Up } from '../../assets/images/Up.svg';
 
 export const TopBox = styled.div`
-    height: 250px;
+    height: 18rem;
     width: 100%;
     background-color: rgba(195, 233, 255, 0.5);
     display: flex;
@@ -28,5 +28,5 @@ export const BoxDetail = styled.div`
     justify-content: center;
     height: 25%;
     flex-direction: column;
-    margin: 100px;
+    margin: 7rem;
 `;
