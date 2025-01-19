36545a7ac3a7efd1767f2ca20fcbe62a0ac1b199
soobin <subin3620@naver.com>
2025-01-19 18:21:39 +0900

    :recycle: Refactor teambox Responsive UI
    #331 팀박스 반응형 사이즈 구현

--- gongjakso/src/pages/ProfilePage/ProfilePageStyled.jsx
+++ gongjakso/src/pages/ProfilePage/ProfilePageStyled.jsx
@@ -13,6 +13,13 @@ export const TopBox = styled.div`
     display: flex;
     justify-content: center;
     align-items: flex-end;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        background-color: transparent;
+        height: 12rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        height: 14rem;
+    }
 `;
 
 export const DetailBox = styled.div`
@@ -21,6 +28,14 @@ export const DetailBox = styled.div`
     text-align: left;
     display: flex;
     justify-content: space-between;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        flex-direction: column;
+        align-items: center;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        flex-direction: column;
+        align-items: center;
+    }
 `;
 
 export const InfoBox = styled.div`
@@ -33,6 +48,14 @@ export const InfoBox = styled.div`
     justify-content: space-between;
     flex-direction: column;
     margin-left: -28rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        margin-left: 0;
+        align-items: center;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        margin-left: 0;
+        align-items: center;
+    }
 `;
 
 //이름
@@ -40,6 +63,12 @@ export const NameTitle = styled.p`
     font-size: ${({ theme }) => theme.fontSize.ll};
     font-family: 'PreBold';
     margin-bottom: 0.3125rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 1.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1.75rem;
+    }
 `;
 
 //학과
@@ -50,6 +79,16 @@ export const MajorTitle = styled.p`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 1rem;
+        width: 100%;
+        text-align: center;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1.25rem;
+        width: 100%;
+        text-align: center;
+    }
 `;
 
 //소제목
@@ -60,6 +99,14 @@ export const SubTitle = styled.p`
     padding-bottom: 1.5625rem;
     display: flex;
     align-items: center;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.938rem;
+        width: 100%;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1.125rem;
+        width: 100%;
+    }
 `;
 
 //프로필 이미지
@@ -71,10 +118,22 @@ export const ProfileImage = styled(My_page_big)`
     display: flex;
     justify-content: center;
     align-items: center;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 10rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 11rem;
+    }
 `;
 
 export const EditImage = styled(Edit)`
     height: 1.75rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        height: 1.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        height: 1.6rem;
+    }
 `;
 
 export const ArrowImage = styled(Arrow)`
@@ -82,6 +141,14 @@ export const ArrowImage = styled(Arrow)`
     display: flex;
     align-items: center;
     height: 1.5625rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 3rem;
+        height: 1.25rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 3.5rem;
+        height: 1.4rem;
+    }
 `;
 
 //나의 포트폴리오 박스
@@ -97,11 +164,29 @@ export const PortfolioBox = styled.button`
     transform: translateY(150%);
     border-radius: 1.5625rem;
     margin-left: 3.125rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20rem;
+        height: 3rem;
+        font-size: 0.875rem;
+        margin-left: 0;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 24rem;
+        height: 3.2rem;
+        font-size: 1rem;
+        margin-left: 0;
+    }
 `;
 
 export const GlobalBox = styled.div`
     width: 100%;
     margin: 11.25rem auto 0 auto; // 가운데 정렬
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        margin: 5rem auto 0 auto;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        margin: 7rem auto 0 auto;
+    }
 `;
 
 export const BoxDetail = styled.div`
@@ -111,6 +196,12 @@ export const BoxDetail = styled.div`
     justify-content: center;
     align-items: center;
     padding-bottom: 5.625rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        padding-bottom: 3rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        padding-bottom: 4rem;
+    }
 `;
 
 export const NoPortfolio = styled.div`
@@ -129,6 +220,18 @@ export const NoPortfolio = styled.div`
     gap: 4rem;
     line-height: 2.088rem;
     font-family: 'PreRegular';
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20.75rem;
+        height: 20rem;
+        padding: 2rem;
+        font-size: 0.875rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 35rem;
+        height: 22rem;
+        padding: 2.5rem;
+        font-size: 1rem;
+    }
 `;
 
 export const MakePortfolioBtn = styled.button`
@@ -144,11 +247,27 @@ export const MakePortfolioBtn = styled.button`
     border-radius: 1rem;
     font-size: ${({ theme }) => theme.fontSize.mdd};
     font-family: 'PreMedium';
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 15rem;
+        height: 3.5rem;
+        font-size: 0.875rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 20rem;
+        height: 4rem;
+        font-size: 1rem;
+    }
 `;
 
 export const PortfolioContent = styled.div`
     font-size: 1rem;
     font-weight: bold;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.875rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1rem;
+    }
 `;
 
 export const ViewPortfolioBtn = styled.button`
@@ -159,6 +278,14 @@ export const ViewPortfolioBtn = styled.button`
     padding: 0.5rem 1rem;
     cursor: pointer;
     font-size: 0.875rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+        padding: 0.4rem 0.8rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+        padding: 0.5rem 1rem;
+    }
 `;
 
 export const PortfolioTitle = styled.span`
@@ -168,6 +295,14 @@ export const PortfolioTitle = styled.span`
     text-overflow: ellipsis;
     max-width: 43.75rem;
     font-size: ${({ theme }) => theme.fontSize.md};
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.875rem;
+        max-width: 20rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1rem;
+        max-width: 30rem;
+    }
 `;
 
 export const PortfolioButtons = styled.div`
@@ -175,6 +310,12 @@ export const PortfolioButtons = styled.div`
     justify-content: flex-end;
     gap: 0.5rem;
     flex-direction: row;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        gap: 0.3rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        gap: 0.4rem;
+    }
 `;
 
 export const EditPortfolioButton = styled.button`
@@ -185,6 +326,14 @@ export const EditPortfolioButton = styled.button`
     width: 3.5rem;
     height: 3.5rem;
     transform: scale(0.8);
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 2.5rem;
+        height: 2.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 3rem;
+        height: 3rem;
+    }
 `;
 
 export const DeletePortfolioButton = styled.button`
@@ -195,6 +344,14 @@ export const DeletePortfolioButton = styled.button`
     width: 3.6rem;
     height: 3.6rem;
     transform: scale(0.8);
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 2.5rem;
+        height: 2.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 3rem;
+        height: 3rem;
+    }
 `;
 
 export const PortfolioContainer = styled.div`
@@ -209,12 +366,26 @@ export const PortfolioContainer = styled.div`
     background: #e5f5ff;
     padding: 1.5rem 3rem;
     box-sizing: border-box;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20.75rem;
+        padding: 1rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 35rem;
+        padding: 1.5rem;
+    }
 `;
 
 export const PortfolioList = styled.div`
     display: flex;
     flex-direction: column;
     gap: 1rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        gap: 0.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        gap: 0.75rem;
+    }
 `;
 
 export const SubContainer = styled.div`
@@ -222,6 +393,12 @@ export const SubContainer = styled.div`
     justify-content: space-between;
     align-items: center;
     width: 71.875rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20.75rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 35rem;
+    }
 `;
 
 export const SubTitleContainer = styled.div`
@@ -230,6 +407,12 @@ export const SubTitleContainer = styled.div`
     flex-direction: column;
     align-items: center;
     width: 71.875rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20.75rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 35rem;
+    }
 `;
 
 export const Plus = styled.div`
@@ -240,6 +423,14 @@ export const Plus = styled.div`
     background-size: contain;
     cursor: pointer;
     margin-bottom: 1.5625rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 1.125rem;
+        height: 1.125rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 1.25rem;
+        height: 1.25rem;
+    }
 `;
 
 export const LinkInfo = styled.div`
@@ -250,6 +441,12 @@ export const LinkInfo = styled.div`
     font-weight: 600;
     text-align: left;
     gap: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.875rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1rem;
+    }
 `;
 
 export const LinkDetail = styled.div`
@@ -259,6 +456,12 @@ export const LinkDetail = styled.div`
     font-weight: 500;
     text-align: left;
     margin-top: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+    }
 `;
 
 export const PortfolioInfo = styled.div`
@@ -266,4 +469,10 @@ export const PortfolioInfo = styled.div`
     width: 71.875rem;
     gap: 0.5rem;
     flex-direction: column;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 20.75rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        width: 35rem;
+    }
 `;
--- gongjakso/src/pages/TeamBox/TeamBoxStyled.jsx
+++ gongjakso/src/pages/TeamBox/TeamBoxStyled.jsx
@@ -21,6 +21,16 @@ export const Box = styled.div`
     border-radius: 0.9375rem;
     padding: 1.25rem 3.125rem;
     margin: 0.625rem 0rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        max-width: 20.75rem;
+        height: auto;
+        padding: 1rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        max-width: 35rem;
+        height: auto;
+        padding: 1rem 2rem;
+    }
 `;
 
 export const Title = styled.p`
@@ -34,6 +44,16 @@ export const Title = styled.p`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 1rem;
+        width: 100%;
+        order: 1;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1.25rem;
+        width: 100%;
+        margin-top: 0.55rem;
+    }
 `;
 
 export const subTitle = styled.p`
@@ -42,6 +62,19 @@ export const subTitle = styled.p`
     display: flex;
     align-items: center;
     justify-content: center;
+    margin-top: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.875rem;
+        margin-top: 0.5rem;
+        order: 2;
+        width: 100%;
+        justify-content: left;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 1rem;
+        margin-top: 1.05rem;
+        justify-content: left;
+    }
 `;
 
 //프로필페이지-제목 등
@@ -51,12 +84,19 @@ export const BoxTopDetail = styled.div`
     align-items: center;
     justify-content: space-between;
     text-align: left;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        flex-direction: column;
+        align-items: flex-start;
+    }
 `;
 
 export const MainBox = styled.div`
     width: auto;
     display: flex;
     justify-content: space-between;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        flex-direction: column;
+    }
 `;
 
 export const BottomBox = styled.div`
@@ -70,6 +110,13 @@ export const SubBox = styled.div`
     display: flex;
     justify-content: space-between;
     gap: 1rem;
+    margin-top: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 100%;
+        justify-content: flex-start;
+        margin-top: 0.5rem;
+        order: 3;
+    }
 `;
 
 //프로필페이지-파트명
@@ -80,12 +127,19 @@ export const BoxBottomDetail = styled.div`
     justify-content: space-between;
     align-items: center;
     text-align: left;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        flex-direction: column;
+        align-items: flex-start;
+    }
 `;
 
 //프로필 이미지
 export const UnScrapImage = styled(UnScrap)`
     width: 1.5625rem;
     margin-right: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 1.25rem;
+    }
 `;
 
 //마감 일수 이미지
@@ -93,6 +147,9 @@ export const FireImage = styled(Fire)`
     width: 1.5rem;
     align-items: center;
     margin-right: 0.5rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        width: 1.25rem;
+    }
 `;
 
 export const CloseImage = styled(CloseWhite)`
@@ -118,6 +175,17 @@ export const DeadLine = styled.div`
     font-weight: 600;
     text-align: center;
     color: ${({ theme }) => theme.Purple};
+    /* 모바일·태블릿에서만 축소 */
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+        height: 2.2rem;
+        min-width: 6rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+        height: 2.4rem;
+        min-width: 7rem;
+    }
 `;
 
 //스크랩 횟수
@@ -134,6 +202,17 @@ export const ScrapNum = styled.div`
     font-weight: 600;
     text-align: center;
     color: ${({ theme }) => theme.Green};
+    /* 모바일·태블릿에서만 축소 */
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+        height: 2.2rem;
+        min-width: 6rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+        height: 2.4rem;
+        min-width: 7rem;
+    }
 `;
 
 //합류 대기중
@@ -150,10 +229,23 @@ export const WaitingJoin = styled.div`
               ? theme.LightGrey
               : theme.Light1};
     border-radius: 3.125rem;
-    font-size: ${({ theme }) => theme.fontSize.md};
+    font-size: ${({ theme }) => theme.fontSize.m};
     font-weight: 600;
     text-align: center;
     color: ${({ theme }) => theme.mainFont2};
+    /* 모바일·태블릿에서만 축소 */
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+        height: 2.2rem;
+        width: 5rem;
+        margin-bottom: 0.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+        height: 2.4rem;
+        width: 7rem;
+        margin-bottom: 0.5rem;
+    }
 `;
 
 //활동 중 or 활동 중단
@@ -176,21 +268,50 @@ export const ActivityStatus = styled.div`
                 ? 'pointer'
                 : 'not-allowed'
             : 'not-allowed'};
+    /* 모바일·태블릿에서만 축소 */
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 0.75rem;
+        height: 2.2rem;
+        width: 5rem;
+        margin-bottom: 0.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        font-size: 0.875rem;
+        height: 2.4rem;
+        width: 7rem;
+        margin-bottom: 0.5rem;
+    }
 `;
 
 //검은색 둥근 틀
 export const RoundForm = styled.div`
+    display: inline-flex;
     min-width: 8rem;
     height: 2.6rem;
-    display: flex;
     align-items: center;
     justify-content: center;
     background: black;
     border-radius: 1.2rem;
-    font-size: ${({ theme }) => theme.fontSize.mm};
+    font-size: ${({ theme }) => theme.fontSize.m};
     font-weight: 500;
     color: ${({ theme }) => theme.mainFont2};
     margin-right: 0.8rem;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        /* 모바일에서 축소 */
+        min-width: 4rem;
+        height: 2rem;
+        font-size: 0.75rem;
+        margin-right: 0.4rem;
+        padding: 0 0.5rem;
+        margin-bottom: 0.5rem;
+    }
+    @media screen and (min-width: 550px) and (max-width: 1023px) {
+        /* 태블릿에서 축소 */
+        min-width: 5rem;
+        height: 2.2rem;
+        font-size: 0.875rem;
+        margin-bottom: 0.5rem;
+    }
 `;
 
 //프로필페이지-자세히보기
@@ -201,6 +322,9 @@ export const MoreDetail = styled(ArrowDetail)`
     transform: translateY(-50%);
     height: 100%;
     z-index: 3;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        right: 0.5rem;
+    }
 `;
 
 //프로필페이지 -> 모집 마감, 연장, 취소 오버레이
@@ -220,4 +344,7 @@ export const DeadlineOverlay = styled.div`
     justify-content: center;
     align-items: center;
     color: white;
+    @media screen and (min-width: 375px) and (max-width: 549px) {
+        font-size: 1rem;
+    }
 `;
